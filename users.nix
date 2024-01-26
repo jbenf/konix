e90d8d472d6b4e8be9a37d61d34ca6bd721defa0
Julian Feja <julian@feja.eu>
2024-01-26 16:52:41 +0100

    extraRules updated 2

--- users.nix
+++ users.nix
@@ -54,10 +54,14 @@
   '';
 
   security.sudo.extraRules = [
-    { groups = [ "users" ]; runAs = "root"; commands = [ "/usr/bin/systemctl start nix-gc.service" ]; }
-    { groups = [ "users" ]; runAs = "root"; commands = [ "/usr/bin/systemctl start nix-optimise.service" ]; }
-    { groups = [ "users" ]; runAs = "root"; commands = [ "/usr/bin/systemctl start nixos-upgrade.service" ]; }
-    { groups = [ "users" ]; runAs = "root"; commands = [ "/usr/bin/systemctl start konix-flake-update.service" ]; }
+    { groups = [ "users" ]; runAs = "root"; 
+      commands = [ 
+        { command = "/usr/bin/systemctl start nix-gc.service"; options = [ "NOPASSWD" ]; }
+        { command = "/usr/bin/systemctl start nix-optimise.service"; options = [ "NOPASSWD" ]; }
+        { command = "/usr/bin/systemctl start nixos-upgrade.service"; options = [ "NOPASSWD" ]; }
+        { command = "/usr/bin/systemctl start konix-flake-update.service"; options = [ "NOPASSWD" ]; }
+      ]; 
+    }
   ]; 
 
 }
